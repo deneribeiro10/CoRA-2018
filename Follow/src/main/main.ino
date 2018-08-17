57d3a30adf0a5b7c2f594a644324d17afed63524
Dener Vieira <deneribeiro10@hotmail.com>
2018-08-17 15:50:18 -0300

    segue linha com dois sensores e tem estados

--- Follow/src/main/main.ino
+++ Follow/src/main/main.ino
@@ -1,27 +1,63 @@
 #include "C:\Users\DELL\Desktop\Coltec 2018\CORA\CoRA-2018\Follow\lib\Robot.h"
 
+#define KP 255
+#define VEL_MAX 200
+
+enum state {
+  waiting,
+  follow
+};
+
 Robot *sheep = new Robot();
+int sensors[] = {0,1,2,3,4};
+int btn = 12;
+bool button_last = false;
+state robot;
 
 void setup() {
   Serial.begin(9600);
-  
+  robot = waiting;
+  pinMode(btn, INPUT);
   sheep->create(); 
-  
 }
 
 void loop() {
-  int s1 = analogRead(2);
-  int s2 = analogRead(3);
+  bool button;
+  bool s[5];
+  int follow_state;
 
-  if(s1 >= 512) {
-    sheep->startMotorRight(150);
-  } else {
-    sheep->stopMotorRight();
-  }
-  if(s2 >= 512) {
-    sheep->startMotorLeft(150);
-  } else {
-    sheep->stopMotorLeft();
+  switch(robot) {
+    case waiting: 
+      button = digitalRead(btn);
+      if(button == false && button_last == true) {
+        robot = follow;
+      }
+      button_last = button;
+      break;
+      
+    case follow:
+      for(int i=0; i<5; i++) {
+        s[i] = analogRead( sensors[i] ) < 150;
+      }
+      follow_state = getState(s);
+      if(follow_state > 0) {
+        sheep->stopMotorLeft();
+        sheep->startMotorRight(VEL_MAX);
+      } else if(follow_state < 0) {
+        sheep->startMotorLeft(VEL_MAX);
+        sheep->stopMotorRight();
+      }
+      break;
   }
 }
+
+int getState(bool s[]) {
+    int st = 1;
+      if(s[1]) {
+        st = 1;
+      } else if(s[3]) {
+        st = -1;
+      }
+    return st;
+  }
 
